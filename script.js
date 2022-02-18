f0509e2b26feeeba022d638b0826d55771a923d4
Igor <ig.zab.ru@gmail.com>
2022-02-18 15:44:10 +0100

    final build

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ document.querySelector('.burger').addEventListener('click', () => {
   document.querySelector('.burger').classList.toggle('active')
   document.body.classList.toggle('fixed-position')
 })
+
+document.querySelectorAll('.spoiler__trigger').forEach((item) => {
+  item.addEventListener('click', () => {
+    const parent = item.parentNode
+    if (parent.classList.contains('spoiler__item--active')) {
+      parent.classList.remove('spoiler__item--active')
+    } else {
+      document.querySelectorAll('.spoiler__item').forEach((child) => {
+        child.classList.remove('spoiler__item--active')
+        parent.classList.add('spoiler__item--active')
+      })
+    }
+  })
+})
